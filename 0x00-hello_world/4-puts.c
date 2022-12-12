42d8d1077991885955313d4fe84199dbfbf18023
anthony <afrikind99@gmail.com>
2022-12-12 14:56:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
- 
 /**
 * main - Entry Point
 *
@@ -8,5 +7,5 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");return (0);
+puts("\"Programming is like building a multilingual puzzle"); return (0);
 }

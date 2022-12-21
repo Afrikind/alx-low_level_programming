1ed95556b9b9324e94a0948fa10357131d90cd0a
anthony <afrikind99@gmail.com>
2022-12-21 15:37:53 -0800

    another One on Pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
+/**
+ * File: main.h
+ * main.h = MAIN.H
+ * author: anthony
+ * Desc: main.h descriptions
+ * Used in the 0x06. C - more pointers, arrays and strings directory.
+ */
 
-int _putchar(char c); 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -12,7 +17,10 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif

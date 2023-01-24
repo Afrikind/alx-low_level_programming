6b34eb6c4590b0de1a727343c9113c371fcbe707
anthony <afrikind99@gmail.com>
2023-01-24 13:53:33 -0800

    More codings

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,23 +6,23 @@
 #include <stdio.h>
 
 /**
- * stuct op - Struct op
- * @op - The operator
- * @f - The function in associated
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 int (*get_op_func(char *s))(int, int);
 
 #endif
